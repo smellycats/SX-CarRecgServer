8c3d323dd13d70594c5a87c5ccb981f45634ae61
SmellyCat <smellycat2014@foxmail.com>
2015-10-13 00:47:43 +0800

    Add auth methods

--- car_recg/views.py
+++ car_recg/views.py
@@ -83,6 +83,160 @@ class Index(Resource):
             'state_url': '%sv1/state' % (request.url_root)
         }, 200, {'Cache-Control': 'public, max-age=60, s-maxage=60'}
 
+class User(Resource):
+    decorators = [limiter.limit("5000/hours")]
+
+    @verify_addr
+    @verify_token
+    @verify_scope('user_get')
+    def get(self, user_id):
+        user = Users.query.filter_by(id=user_id, banned=0).first()
+        if user:
+            return {'id': user.id,
+                    'username': user.username,
+                    'scope': user.scope,
+                    'date_created': str(user.date_created),
+                    'date_modified': str(user.date_modified),
+                    'banned': user.banned}, 200
+        else:
+            return {}, 404
+
+    @verify_addr
+    @verify_token
+    @verify_scope('user_patch')
+    def post(self, user_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('scope', type=unicode, required=True,
+                            help='A scope field is require', location='json')
+        args = parser.parse_args()
+
+        # 所有权限范围
+        all_scope = set()
+        for i in Scope.query.all():
+            all_scope.add(i.name)
+        # 授予的权限范围
+        request_scope = set(request.json.get('scope', u'null').split(','))
+        # 求交集后的权限
+        u_scope = ','.join(all_scope & request_scope)
+
+        db.session.query(Users).filter_by(id=user_id).update(
+            {'scope': u_scope, 'date_modified': arrow.now().datetime})
+        db.session.commit()
+
+        user = Users.query.filter_by(id=user_id).first()
+        app.config['SCOPE_USER'][user.id] = set(user.scope.split(','))
+
+        return {
+            'id': user.id,
+            'username': user.username,
+            'scope': user.scope,
+            'date_created': str(user.date_created),
+            'date_modified': str(user.date_modified),
+            'banned': user.banned
+        }, 201
+
+
+class UserList(Resource):
+    decorators = [limiter.limit("50/minute")]
+
+    @verify_addr
+    @verify_token
+    @verify_scope('user_post')
+    def post(self):
+        if not request.json.get('username', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+        if not request.json.get('password', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+
+        user = Users.query.filter_by(username=request.json['username'],
+                                     banned=0).first()
+        if not user:
+            password_hash = sha256_crypt.encrypt(request.json['password'],
+                                                 rounds=app.config['ROUNDS'])
+            # 所有权限范围
+            all_scope = set()
+            for i in Scope.query.all():
+                all_scope.add(i.name)
+            # 授予的权限范围
+            request_scope = set(request.json.get('scope', u'null').split(','))
+            # 求交集后的权限
+            u_scope = ','.join(all_scope & request_scope)
+            u = Users(username=request.json['username'],
+                      password=password_hash, scope=u_scope, banned=0)
+            db.session.add(u)
+            db.session.commit()
+            return {
+                'id': u.id,
+                'username': u.username,
+                'scope': u.scope,
+                'date_created': str(u.date_created),
+                'date_modified': str(u.date_modified),
+                'banned': u.banned
+            }, 201
+        else:
+            return {'message': 'username is already esist'}, 422
+
+
+class ScopeList(Resource):
+
+    @verify_addr
+    @verify_token
+    @verify_scope('scope_get')
+    def get(self):
+        scope = Scope.query.all()
+        items = []
+        for i in scope:
+            items.append(row2dict(i))
+        return {'total_count': len(items), 'items': items}, 200
+
+
+def get_uid():
+    g.uid = -1
+    g.scope = ''
+    try:
+        user = Users.query.filter_by(username=request.json.get('username', ''),
+                                     banned=0).first()
+    except Exception as e:
+        logger.error(e)
+        raise
+    if user:
+        if sha256_crypt.verify(request.json.get('password', ''), user.password):
+            g.uid = user.id
+            g.scope = user.scope
+            return str(g.uid)
+    return request.remote_addr
+
+
+class TokenList(Resource):
+    decorators = [limiter.limit("5/hour", get_uid)]
+
+    @verify_addr
+    def post(self):
+        if not request.json.get('username', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+        if not request.json.get('password', None):
+            error = {'resource': 'Token', 'field': 'username',
+                     'code': 'missing_field'}
+            return {'message': 'Validation Failed', 'errors': error}, 422
+        if g.uid == -1:
+            return {'message': 'username or password error'}, 422
+        s = Serializer(app.config['SECRET_KEY'],
+                       expires_in=app.config['EXPIRES'])
+        token = s.dumps({'uid': g.uid, 'scope': g.scope.split(',')})
+        return {
+            'uid': g.uid,
+            'access_token': token,
+            'token_type': 'self',
+            'scope': g.scope,
+            'expires_in': app.config['EXPIRES']
+        }, 201, {'Cache-Control': 'no-store', 'Pragma': 'no-cache'}
+
 
 class RecgListApiV1(Resource):
 
@@ -113,7 +267,7 @@ class RecgListApiV1(Resource):
         app.config['RECGQUE'].put((10, request.json, que, imgpath))
 
         try:
-            recginfo = que.get(timeout=15)
+            recginfo = que.get(timeout=app.config['TIMEOUT'])
 
             os.remove(imgpath)
         except Queue.Empty:
@@ -171,6 +325,10 @@ class UploadRecgListApiV1(Resource):
             return {'coord': r['coord'], 'recginfo': recginfo}, 201
 
 api.add_resource(Index, '/')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserList, '/user')
+api.add_resource(ScopeList, '/user/scope')
+api.add_resource(TokenList, '/token')
 api.add_resource(RecgListApiV1, '/v1/recg')
 api.add_resource(StateListApiV1, '/v1/state')
 api.add_resource(UploadRecgListApiV1, '/v1/uploadrecg')
